d3599dac61d8c56676585a48bb432a3e663c1307
Wendebourg Joran <wjoran@compgpu4.iwr.uni-heidelberg.de>
2024-05-13 16:38:51 +0200

    add VAE architecture

--- fff/model/__init__.py
+++ fff/model/__init__.py
@@ -1,5 +1,6 @@
 from .auto_encoder import FullyConnectedNetwork, FullyConnectedNetworkHParams
 from .conv_auto_encoder import ConvolutionalNeuralNetwork, ConvolutionalNeuralNetworkHParams
+from .var_res_net import VarResNet
 from .injective_flow import InjectiveFlow, InjectiveFlowHParams
 from .res_net import ResNet, ResNetHParams
 from .en_graph import ENGNN, ENGNNH, ENGNNHParams
