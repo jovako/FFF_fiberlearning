161da0d14342c7ba0417826f52e066c1ceddb300
Joran Wendebourg <jorval.k@gmail.com>
2024-06-13 17:03:13 +0200

    new plots, models and configs

--- fff/fif.py
+++ fff/fif.py
@@ -50,6 +50,10 @@ class FreeFormInjectiveFlow(FreeFormBase):
                     self.Teacher = FreeFormInjectiveFlow.load_from_checkpoint(
                             "GTs/16Mnist_F3F/checkpoints/best.ckpt"
                     )
+                elif hparams["data_set"]["path"] in ["16Mnist_F5F"]:
+                    self.Teacher = FreeFormInjectiveFlow.load_from_checkpoint(
+                            "GTs/16Mnist_F5F/checkpoints/last.ckpt"
+                    )
                 elif hparams["data_set"]["path"]=="28Mnist_AE5":
                     self.Teacher = FreeFormInjectiveFlow.load_from_checkpoint(
                             "GTs/28Mnist_AE5/checkpoints/last.ckpt"
